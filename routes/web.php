78d24cedf2ea684d627759cff4196321ed273756
Shahenda <shahenda.cs.99@gmail.com>
2023-08-15 17:49:33 +0300

    add user dashboard, admin dashboard, user edit, delete, update routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\UserController;
 use App\Http\Livewire\Users\UserForm;
 use App\Http\Livewire\Users\UserList;
 
@@ -22,7 +23,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/user/dashboard', function(){
+    return view('users.dashboard');
+})->name('user.dashboard');
 
+Route::get('/admin/dashboard', function(){
+    return view('admin.dashboard');
+})->name('admin.dashboard');
 
 Route::get('login', [AuthController::class, 'index'])->name('login');
 Route::post('post-login', [AuthController::class, 'postLogin'])->name('login.post');
@@ -31,9 +38,14 @@ Route::post('post-registration', [AuthController::class, 'postRegistration'])->n
 Route::get('dashboard', [AuthController::class, 'dashboard']);
 Route::get('logout', [AuthController::class, 'logout'])->name('logout');
 
+Route::get('user', [UserController::class, 'index'])->name('user.index');
+Route::get('user/edit/{id}', [UserController::class, 'edit'])->name('user.edit');
+Route::put('user/{id}', [UserController::class, 'update'])->name('user.update');
+Route::delete('user/{id}', [UserController::class, 'destroy'])->name('user.destroy');
+Route::get('user/{id}', [UserController::class, 'show'])->name('user.show');
 
-Route::get('user', UserList::class)->name('user.index');
-// Route::get('user/create', UserForm::class)->name('user.create');
-Route::get('user/edit/{user}', UserForm::class)->name('user.edit');
 
-
+// Route::resource('user','UserController');
+// Route::get('user', UserList::class)->name('user.index');
+// // Route::get('user/create', UserForm::class)->name('user.create');
+// Route::get('user/edit/{user}', UserForm::class)->name('user.edit');
